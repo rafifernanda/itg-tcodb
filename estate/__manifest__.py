c937642c7e0dd01a1ce57cbbae1a5dba7e6adeb0
rafifernanda <muhammadrafifernanda@gmail.com>
2024-05-22 06:52:17 +0700

    chapter 5 - Actions & Menu

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -4,5 +4,5 @@
     "license": "OPL-1",
     "application": True,
     "depends": ["base"],
-    "data": ["data/demo.xml", "security/res_groups.xml", "security/ir.access.model.csv"]
+    "data": ["data/demo.xml", "security/res_groups.xml", "security/ir.access.model.csv", "views/estate_property_views.xml","views/estate_menus.xml",]
 }
