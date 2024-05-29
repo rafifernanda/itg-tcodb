61eae194d1222c481421d76ce684ebce538ec06a
Rangga Raditya Putra Pratama <rangga.raditya@itgroupinc.asia>
2024-05-29 22:02:31 +0700

    chapter 5

---
+++ estate/models/estate_property.py
@@ -0,0 +1,34 @@
+from odoo import fields, models
+
+
+class EstateProperty(models.Model):
+
+    # ---------------------------------------- Private Attributes ---------------------------------
+
+    _name = "estate.property"
+    _description = "Real Estate Property"
+
+    # --------------------------------------- Fields Declaration ----------------------------------
+
+    # Basic
+    name = fields.Char("Title", required=True)
+    description = fields.Text("Description")
+    postcode = fields.Char("Postcode")
+    date_availability = fields.Date("Available From")
+    expected_price = fields.Float("Expected Price", required=True)
+    selling_price = fields.Float("Selling Price")
+    bedrooms = fields.Integer("Bedrooms")
+    living_area = fields.Integer("Living Area (sqm)")
+    facades = fields.Integer("Facades")
+    garage = fields.Boolean("Garage")
+    garden = fields.Boolean("Garden")
+    garden_area = fields.Integer("Garden Area (sqm)")
+    garden_orientation = fields.Selection(
+        selection=[
+            ("N", "North"),
+            ("S", "South"),
+            ("E", "East"),
+            ("W", "West"),
+        ],
+        string="Garden Orientation",
+    )
