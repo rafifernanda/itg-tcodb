6f481f8b7d5e378a94e2e6c4d013294d50040b42
Rangga Raditya Putra Pratama <rangga.raditya@itgroupinc.asia>
2024-05-29 23:02:03 +0700

    chapter 6 - field view attrs

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,5 +1,5 @@
 from odoo import fields, models
-
+from dateutil.relativedelta import relativedelta
 
 class EstateProperty(models.Model):
 
@@ -8,16 +8,21 @@ class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Real Estate Property"
 
+    # ---------------------------------------- Default Methods ------------------------------------
+
+    def _default_date_availability(self):
+        return fields.Date.context_today(self) + relativedelta(months=3)
+
     # --------------------------------------- Fields Declaration ----------------------------------
 
     # Basic
     name = fields.Char("Title", required=True)
     description = fields.Text("Description")
     postcode = fields.Char("Postcode")
-    date_availability = fields.Date("Available From")
+    date_availability = fields.Date("Available From", copy=False, default=lambda self: self._default_date_availability())
     expected_price = fields.Float("Expected Price", required=True)
-    selling_price = fields.Float("Selling Price")
-    bedrooms = fields.Integer("Bedrooms")
+    selling_price = fields.Float("Selling Price", readonly=True, copy=False)
+    bedrooms = fields.Integer("Bedrooms", default=2)
     living_area = fields.Integer("Living Area (sqm)")
     facades = fields.Integer("Facades")
     garage = fields.Boolean("Garage")
@@ -32,3 +37,18 @@ class EstateProperty(models.Model):
         ],
         string="Garden Orientation",
     )
+    active = fields.Boolean("Active", default=True)
+    state =  fields.Selection(
+        selection=[
+            ("new", "New"),
+            ("offer_received", "Offer Received"),
+            ("offer_accepted", "Offer Accepted"),
+            ("sold", "Sold"),
+            ("canceled", "Canceled"),
+        ],
+        string="Status",
+        required=True,
+        copy=False,
+        default="new",
+    )
+
