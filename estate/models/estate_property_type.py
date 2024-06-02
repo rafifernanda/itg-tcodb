f7b0425a5efc40e562030f7147b6b9717319e2aa
Rangga Raditya Putra Pratama <rangga.raditya@itgroupinc.asia>
2024-06-02 15:42:01 +0700

    chapter 7 - many 2 one

--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -12,10 +12,3 @@ class EstatePropertyType(models.Model):
 
     # Basic
     name = fields.Char("Name", required=True)
-
-    # Relational
-    property_ids = fields.One2many("estate.property", "property_type_id", string="Properties")
-
-    def _compute_name(self):
-        for record in self:
-            record.name = "test"
