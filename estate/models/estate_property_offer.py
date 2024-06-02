a43cc7c16f8ccb93cd158ce97246b2637d1de161
Rangga Raditya Putra Pratama <rangga.raditya@itgroupinc.asia>
2024-06-02 16:20:19 +0700

    chapter 7 - one 2 many

---
+++ estate/models/estate_property_offer.py
@@ -0,0 +1,27 @@
+from odoo import fields, models
+
+
+class EstatePropertyOffer(models.Model):
+
+    # ---------------------------------------- Private Attributes ---------------------------------
+
+    _name = "estate.property.offer"
+    _description = "Real Estate Property Offer"
+
+    # --------------------------------------- Fields Declaration ----------------------------------
+
+    price = fields.Float("Price", required=True)
+    state = fields.Selection(
+        selection=[
+            ("accepted", "Accepted"),
+            ("refused", "Refused"),
+        ],
+        string="Status",
+        copy=False,
+        default=False,
+    )
+
+    # Relational
+    partner_id = fields.Many2one("res.partner", string="Partner", required=True)
+    property_id = fields.Many2one("estate.property", string="Property", required=True)
+    
