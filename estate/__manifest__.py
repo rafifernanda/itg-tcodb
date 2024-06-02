2c2de2e8930c5c2839e41fbb1dd6eecc33b6ae73
Rangga Raditya Putra Pratama <rangga.raditya@itgroupinc.asia>
2024-06-02 15:51:01 +0700

    chapter 7 - many 2 one

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -10,6 +10,7 @@
     ],
     "data": [
         "views/estate_property_views.xml",
+		"views/estate_property_type_views.xml",
         "views/estate_menus.xml",
 
         "security/res_groups.xml",
