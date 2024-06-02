68d9d8c8eb40093c398689bc82d43d755ac46986
Rangga Raditya Putra Pratama <rangga.raditya@itgroupinc.asia>
2024-06-02 15:37:05 +0700

    chapter 7 - many 2 one

---
+++ estate/models/estate_property_type.py
@@ -0,0 +1,21 @@
+from odoo import fields, models
+
+
+class EstatePropertyType(models.Model):
+
+    # ---------------------------------------- Private Attributes ---------------------------------
+
+    _name = "estate.property.type"
+    _description = "Real Estate Property Type"
+
+    # --------------------------------------- Fields Declaration ----------------------------------
+
+    # Basic
+    name = fields.Char("Name", required=True)
+
+    # Relational
+    property_ids = fields.One2many("estate.property", "property_type_id", string="Properties")
+
+    def _compute_name(self):
+        for record in self:
+            record.name = "test"
