e4a4ea84a1c3e3d85f1369523862085144b194f6
Rangga Raditya Putra Pratama <rangga.raditya@itgroupinc.asia>
2024-06-03 17:10:40 +0700

    chapter 17

--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import fields, models, api, _
 
 
 class EstatePropertyType(models.Model):
@@ -20,4 +20,26 @@ class EstatePropertyType(models.Model):
     
     # Relational
     property_ids = fields.One2many("estate.property", "property_type_id", string="Properties")
+
+    # Computed (for stat button)
+    offer_count = fields.Integer(string="Offers Count", compute="_compute_offer_count")
+    offer_ids = fields.One2many("estate.property.offer", "property_type_id")
+
+    # ---------------------------------------- Compute methods ------------------------------------
+
+    @api.depends("offer_ids")
+    def _compute_offer_count(self):
+        for rec in self:
+            rec.offer_count = len(rec.offer_ids)
+
+    # ---------------------------------------- Action Methods -------------------------------------
+
+    def action_view_offers(self):
+        return {
+            "name": _("Property Offers"),
+            "type": "ir.actions.act_window",
+            "view_mode": "tree",
+            "res_model": "estate.property.offer",
+            "domain": [("id", "in", self.offer_ids.ids)],
+        }
 
