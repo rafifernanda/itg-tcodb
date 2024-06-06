c39f3e4f5315f9aa1407f1c300ba2a0e6c7f4133
faisal <ahmad.rifa@itgroupinc.asia>
2024-06-06 16:13:31 +0700

    test view 1

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -6,14 +6,10 @@
     "depends": ["crm"],
     "data": [
         # SECURITY
-        "security/res_groups.xml",
         "security/ir.model.access.csv"
         # VIEWS
         'views/estate_property_views.xml',
         'views/estate_menus.xml'
         # MENUS
-    ],
-    "demo": [
-        "demo/demo.xml"
     ]
 }
