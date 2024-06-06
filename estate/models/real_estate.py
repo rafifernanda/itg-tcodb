5b0fd9b0cea868ddc9d13901efe2cb1e0dd12c85
faisal <ahmad.rifa@itgroupinc.asia>
2024-06-06 13:50:42 +0700

    add fields

--- estate/models/real_estate.py
+++ estate/models/real_estate.py
@@ -1,6 +1,8 @@
-from odoo import models
+from odoo import fields,models
 
 class RealEstate(models.Model):
     _name = "real.estate"
     _description = "Test model"
-    
+
+    name = fields.Char(default="House",required=True)
+    price = fields.Float
