a422420f6d5b34ab28236a2eb409a46a7ec57c16
faisal <ahmad.rifa@itgroupinc.asia>
2024-06-11 14:44:53 +0700

    views menu 3

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -5,7 +5,7 @@
     "license":"OEEL-1",
     "depends":["crm"],
     "data":[
-        "security/res_groups.xml"
+        "security/res_groups.xml",
         "security/ir.model.access.csv",
         "views/estate_property_views.xml",
         "views/estate_menus.xml",
