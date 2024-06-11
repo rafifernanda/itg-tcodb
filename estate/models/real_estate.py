458c3bc10ce7502e16b252bfe5387c6201a48644
faisal <ahmad.rifa@itgroupinc.asia>
2024-06-11 10:45:22 +0700

    rebuild fields

--- estate/models/real_estate.py
+++ estate/models/real_estate.py
@@ -3,3 +3,6 @@ from odoo import models
 class RealEstate(models.Model):
     _name = "real.estate"
     _description = "Test model"
+
+    name = fields.Char(default = "House", required=True)
+    price = fields.Float()
