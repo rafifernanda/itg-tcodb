390b365ae09bc41e2af8939b85e5f966a45c02af
faisal <ahmad.rifa@itgroupinc.asia>
2024-06-11 13:26:34 +0700

    rebuild view 1.1

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -5,9 +5,8 @@
     "license": "OEEL-1",
     "depends": ["crm"],
     "data": [
-        "security/res_groups.xml",
+        "security/ir.model.access.csv",
+        "views/estate_property_views.xml",
+        "views/estate_menus.xml",
     ],
-    "demo": [
-        "demo/demo.xml"
-    ]
 }
