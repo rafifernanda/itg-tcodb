81a0d7c70e85dc5c66a0dc017eefc2b258c02a93
faisal <ahmad.rifa@itgroupinc.asia>
2024-06-11 11:11:31 +0700

    rebuild security

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -5,5 +5,10 @@
     "license": "OEEL-1",
     "depends": ["crm"],
     "data": [
+        "security/res_groups.xml"
+        "security/ir.model.access.csv"
+    ],
+    "demo": [
+        "demo/demo.xml"
     ]
 }
